0706c697c4057e9e22c769f8b37f20665360e4dc
Dhruv Popat <dhruvpopat268@gmail.com>
2025-06-04 17:53:06 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
-  console.log(token)
+   alert('token',token)
+
 
   if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', request.url))
