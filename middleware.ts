74f4f13a6f42d50c7b06bb2f491c90fbfff804c9
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-04 16:38:14 +0530

    checking

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get('token')?.value
 
-  if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  
 
-  return NextResponse.next()
-}
+//   if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
 
-export const config = {
-  matcher: ['/dashboard/:path*'], // Protect all /dashboard routes
-}
+//   return NextResponse.next()
+// }
+
+// export const config = {
+//   matcher: ['/dashboard/:path*'], // Protect all /dashboard routes
+// }
