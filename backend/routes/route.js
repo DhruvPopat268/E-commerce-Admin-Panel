e3b105f30a163484c21033805cddc2ceae92a7bc
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-07 18:59:09 +0530

    checking

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -307,9 +307,20 @@ router.put('/:id/status', async (req, res) => {
 });
 
 // Route to register agent's OneSignal player ID
-router.post('/register-agent-player',verifyToken, async (req, res) => {
+router.post('/register-agent-player', async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(403).json({
+      success: false,
+      message: "Access denied. No token provided."
+    });
+  }
+
+  const token = authHeader.split(' ')[1];
   try {
-    const agentId = req.userId;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const agentId = decoded.id
     const {  playerId } = req.body;
     
     if (!agentId || !playerId) {
