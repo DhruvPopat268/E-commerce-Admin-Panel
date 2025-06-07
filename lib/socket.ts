9b64e14c2db1d57d57e01ac080fea02bcf063707
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-07 16:17:12 +0530

    main code

--- lib/socket.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/socket.ts
-import { io } from "socket.io-client";
-
-const socket = io("https://e-commerce-admin-backend.onrender.com", {
-  withCredentials: true,
-});
-
-export default socket;
