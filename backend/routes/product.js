2a4f6f243ad1805f28682c261ea9c0059b7f94cd
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-09 13:07:26 +0530

    checking

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -432,7 +432,14 @@ router.patch("/:id/daily-needs", async (req, res) => {
 });
 
 router.post('/by-tags', async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(403).json({ message: "Access denied. No token provided." });
+  }
+  const token = authHeader.split(' ')[1];
   try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     const { tagName } = req.body;
     
     // Validate input
