2ddadb88e2bc422198dce35a709a13f671dc50ea
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-16 19:27:38 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
-// Serve static uploads
 app.use('/uploads', express.static('uploads'));
 
 // Define routes
