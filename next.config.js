1cefed0d4ef61dc546cbf4bdc21ea51a5d640d51
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-07-17 17:08:48 +0530

    updated

---
+++ next.config.js
@@ -0,0 +1,25 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    middlewarePrefetch: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
